c9101c5b5ed9301eeade7b8354fabb5d4fd8be0c
gbattocletti <g.battocletti@protonmail.com>
2026-03-20 15:52:47 +0100

    Visualize paths and winding numbers

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from braid-controller!")
+import matplotlib.pyplot as plt
 
+from data import grids_2
+from utils import invariants
+from visualization import plot
 
-if __name__ == "__main__":
-    main()
+# Load data
+grids = grids_2.grids
+
+# Convert grids to paths
+paths = invariants.grids2paths(grids)
+plot.plot_paths_3d(paths, show=False)
+
+# Compute winding numbers
+windings_linear = invariants.paths2windings(
+    paths,
+    upscale_factor=20,
+    intermediate_shape="linear",
+)
+plot.plot_windings(windings_linear, show=False)
+windings_spline = invariants.paths2windings(
+    paths,
+    upscale_factor=20,
+    intermediate_shape="spline",
+)
+plot.plot_windings(windings_spline, show=False)
+
+# Show all plots
+plt.show()
--- visualization/plot.py
+++ visualization/plot.py
@@ -7,7 +7,7 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_paths_2d(paths: np.ndarray) -> tuple[plt.Figure, plt.Axes]:
+def plot_paths_2d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
     """
     Plot a list of paths in 2D space.
 
@@ -15,10 +15,15 @@ def plot_paths_2d(paths: np.ndarray) -> tuple[plt.Figure, plt.Axes]:
         paths (np.ndarray): A 3D array of shape (n, 2, m) representing n time steps of
             m agents' positions in 2D space. Each entry paths[t, :, i] gives the
             (x, y) coordinates of agent i at time step t.
+        show (bool, optional): Whether to display the plot. Default is False.
 
     Returns:
         tuple[plt.Figure, plt.Axes]: The figure and axes objects for the plot.
     """
+    # Parse kwargs
+    show = kwargs.get("show", False)
+    figsize = kwargs.get("figsize", np.array([10, 10]))
+
     # Validate inputs
     if not isinstance(paths, np.ndarray):
         raise TypeError("Input 'paths' must be a numpy array.")
@@ -28,18 +33,22 @@ def plot_paths_2d(paths: np.ndarray) -> tuple[plt.Figure, plt.Axes]:
         raise ValueError(
             "The second dimension of 'paths' must be 2 for (x, y) coordinates."
         )
+    if not isinstance(figsize, np.ndarray):
+        if not isinstance(figsize, (tuple, list)):
+            raise TypeError("Input 'figsize' must be a numpy array, tuple, or list.")
+        figsize = np.array(figsize)
 
     # Extract dimensions
     n, _, m = paths.shape
 
     # Create the plot
     colors = plt.cm.get_cmap("tab10", m).colors
-    fig, ax = plt.subplots(figsize=(10, 10))
+    fig, ax = plt.subplots(figsize=figsize / 2.54)
     for t in range(n):
         for i in range(m):
             x = paths[t, 0, i]
             y = paths[t, 1, i]
-            ax.plot(x, y, linewidth=1.3, color=colors[i], label=f"Agent {i}")
+            ax.plot(x, y, linewidth=1.3, color=colors[i], label=f"Agent {i+1}")
 
     ax.set_aspect("equal", "box")
     ax.set_xlim([-1, m])
@@ -52,7 +61,9 @@ def plot_paths_2d(paths: np.ndarray) -> tuple[plt.Figure, plt.Axes]:
     ax.set_title("2D Paths of Agents")
     ax.legend()
     ax.grid(True)
-    plt.show()
+
+    if show is True:
+        plt.show()
 
     return fig, ax
 
@@ -69,6 +80,7 @@ def plot_paths_3d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
             Default is (10, 10).
         pov (list[float], optional): A list of three floats representing the elevation,
             azimuth, and roll angles for the 3D plot's point of view.
+        show (bool, optional): Whether to display the plot. Default is False.
 
     Returns:
         tuple[plt.Figure, plt.Axes]: The figure and axes objects for the plot
@@ -79,8 +91,9 @@ def plot_paths_3d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
         ValueError: If the second dimension of 'paths' is not 2 for (x, y) coordinates.
     """
     # Parse kwargs
-    figsize = kwargs.get("figsize", (10, 10))
+    figsize = kwargs.get("figsize", np.array([10, 10]))
     pov = kwargs.get("pov", [15, 35, 0])
+    show = kwargs.get("show", False)
 
     # Validate inputs
     if not isinstance(paths, np.ndarray):
@@ -91,6 +104,10 @@ def plot_paths_3d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
         raise ValueError(
             "The second dimension of 'paths' must be 2 for (x, y) coordinates."
         )
+    if not isinstance(figsize, np.ndarray):
+        if not isinstance(figsize, (tuple, list)):
+            raise TypeError("Input 'figsize' must be a numpy array, tuple, or list.")
+        figsize = np.array(figsize)
 
     # Preprocess
     n, _, m = paths.shape  # Extract dimensions
@@ -98,13 +115,12 @@ def plot_paths_3d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
     colors = plt.cm.get_cmap("tab10", m).colors  # Define colormaps
 
     # Create the plot
-    figsize = figsize / 2.54  # convert cm to in
-    fig: plt.Figure = plt.figure(figsize=figsize)  # convert cm to in
+    fig: plt.Figure = plt.figure(figsize=figsize / 2.54)
     ax: Axes3D = fig.add_subplot(projection="3d")
     for i in range(m):
         x = paths[:, 0, i]
         y = paths[:, 1, i]
-        ax.plot(x, y, time, linewidth=1.3, color=colors[i], label=f"Agent {i}")
+        ax.plot(x, y, time, linewidth=1.3, color=colors[i], label=f"Agent {i+1}")
 
     # Set limits and aspect
     ax.set_xlim(-1, m)
@@ -117,12 +133,14 @@ def plot_paths_3d(paths: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
     ax.set_zlabel("t")
     ax.set_title("3D Paths of Agents Over Time")
     ax.legend()
-    plt.show()
+
+    if show:
+        plt.show()
 
     return fig, ax
 
 
-def plot_winding_numbers(windings: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
+def plot_windings(windings: np.ndarray, **kwargs) -> tuple[plt.Figure, plt.Axes]:
     """
     Plot the winding numbers of a braid over time.
 
@@ -133,6 +151,7 @@ def plot_winding_numbers(windings: np.ndarray, **kwargs) -> tuple[plt.Figure, pl
         figsize (tuple[float, float], optional): The size of the figure in cm.
         show_legends (bool, optional): Whether to show legends for the plot. Default is
             False.
+        show (bool, optional): Whether to display the plot. Default is False.
 
     Returns:
         tuple[plt.Figure, plt.Axes]: The figure and axes objects for the plot.
@@ -142,14 +161,19 @@ def plot_winding_numbers(windings: np.ndarray, **kwargs) -> tuple[plt.Figure, pl
         ValueError: If the input 'windings' does not have the correct shape.
     """
     # Parse kwargs
-    figsize = kwargs.get("figsize", (10, 10))
+    figsize = kwargs.get("figsize", np.array([10, 10]))
     show_legends = kwargs.get("show_legends", False)
+    show = kwargs.get("show", False)
 
     # Validate inputs
     if not isinstance(windings, np.ndarray):
         raise TypeError("Input 'windings' must be a numpy array.")
     if windings.ndim != 3 or windings.shape[1] != windings.shape[2]:
         raise ValueError("Input 'windings' must be a 3D array of shape (n, m, m).")
+    if not isinstance(figsize, np.ndarray):
+        if not isinstance(figsize, (tuple, list)):
+            raise TypeError("Input 'figsize' must be a numpy array, tuple, or list.")
+        figsize = np.array(figsize)
 
     # Extract dimensions
     n, m, _ = windings.shape
@@ -161,41 +185,52 @@ def plot_winding_numbers(windings: np.ndarray, **kwargs) -> tuple[plt.Figure, pl
     # Create the plot
     plot_cols = 3
     plot_rows = m // 3 + (m % 3 > 0)
-    figsize = figsize / 2.54  # convert cm to in
+    n_plots = plot_rows * plot_cols
     fig: plt.Figure
-    ax: np.ndarray[plt.Axes]
-    fig, ax = plt.subplots(nrows=plot_rows, ncols=plot_cols, figsize=figsize)
-    for i in range(m):
+    axes: np.ndarray[plt.Axes]
+    fig, axes = plt.subplots(
+        nrows=plot_rows,
+        ncols=plot_cols,
+        figsize=figsize / 2.54,
+        sharex=True,
+    )
+    for i in range(n_plots):
         row = i // plot_cols
         col = i % plot_cols
+        ax: plt.Axes = axes[row, col]
+        if i >= m:
+            ax.axis("off")  # Hide unused subplots
+            continue
         for j in range(m):
-            ax[row, col].plot(
+            if j == i:
+                continue  # Skip self-winding numbers
+            ax.plot(
                 time,
                 windings[:, i, j],
-                linewidth=1.3,
+                linewidth=1.5,
                 color=colors[j],
                 label=f"w_{{{i}{j}}}",
             )
 
         # Set labels and title
-        ax[row, col].set_aspect("equal", "box")
-        ax[row, col].set_xlim(0, n - 1)
-        ax[row, col].set_ylim(windings.min() - 1, windings.max() + 1)
-        ax[row, col].grid(
+        ax.set_xlim(0, n - 1)
+        ax.set_ylim(windings.min() - 1, windings.max() + 1)
+        ax.grid(
             True, which="major", linestyle=":", color="gray", linewidth=0.5, zorder=1
         )
-        ax[row, col].grid(
+        ax.grid(
             True, which="minor", linestyle=":", color="gray", linewidth=0.3, zorder=1
         )
-        ax[row, col].minorticks_on()
-        ax[row, col].set_title(f"Agent {i}")
-        ax[row, col].set_xlabel("t")
-        ax[row, col].set_ylabel("$w_{ij}$")
-        ax[row, col].grid(True)
+        ax.minorticks_on()
+        ax.set_title(f"Agent {i+1}")
+        ax.set_xlabel("t")
+        ax.set_ylabel("$w_{ij}$")
+        ax.grid(True)
 
         if show_legends:
-            ax[row, col].legend()
+            ax.legend()
 
+    if show:
         plt.show()
 
-    return fig, ax
+    return fig, axes
