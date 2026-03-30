b9a14049424b2abd8910a723130374c5407ce0c5
gbattocletti <g.battocletti@tudelft.nl>
2026-03-30 15:51:03 +0200

    Complete main sim loop

--- main.py
+++ main.py
@@ -2,92 +2,105 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from core import agent, mpc
-from data import grids_2
+from data import grids_1, grids_2  # pylint: disable=unused-import
 from utils import invariants
 from visualization import plot
 
+# Settings
+DEBUG = True
+data = grids_2
+np.random.seed(1312)
+
+## Preprocessing #######################################################################
 # Load input data
-grids = grids_2.grids
+m = data.m
+grids = data.grids  # topological specification
+x_init = data.x_init
+x_goal = data.x_goal
 
 # Convert grids to paths
 paths = invariants.grids2paths(grids)
 plot.plot_paths_3d(paths, show=False)
 
 # Compute winding numbers
-windings_spline = invariants.paths2windings(
+windings = invariants.paths2windings(
     paths,
-    upscale_factor=20,
+    upscale_factor=10,
     intermediate_shape="spline",
 )
-plot.plot_windings(windings_spline, show=False)
+n_windings = windings.shape[0]  # length of the winding number vector
+plot.plot_windings(windings, show=False)
 
-# Initialize agents' properties
-# NOTE: x_init and x_goal must be coherent with initial and final relative positions in
-# the topological specifications.
-x_init = np.array(
-    [
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-    ]
-)
-x_goal = np.array(
-    [
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-    ]
-)
-
+## Controller Setup ##################################################################
 # Initialize controller's properties
-dt = 1  # s NOTE: also used by the agents
+dt = 0.5  # s
 K = 20  # time steps
 
 # Create agents
-m = paths.shape[-1]
 M = [agent.Agent(i) for i in range(m)]
 for i in range(m):
     M[i].dt = dt
     M[i].x = x_init[i, :]
-    M[i].x_init = x_init[i, :]
     M[i].x_goal = x_goal[i, :]
-    M[i].x_pred = np.zeros([m, m, K])
 
-# Initialize MPC controller
-controller = mpc.MPC()
-controller.dt = dt
-controller.K = K
-controller.m = m
-controller.alpha_u = 1.0
-controller.alpha_goal = 1.0
-controller.alpha_w = 1.0
-controller.R = np.diagonal([1, 1])
+# Initialize MPC controller (local MPC for distributed control architecture)
+mpc_distributed = mpc.MPC()
+mpc_distributed.dt = dt
+mpc_distributed.K = K
+mpc_distributed.m = m
+mpc_distributed.alpha_u = 1.0
+mpc_distributed.alpha_goal = 1.0
+mpc_distributed.alpha_w = 1.0
+mpc_distributed.R = np.diag([1, 1])
+mpc_distributed.u_min = np.array([-1, -1])  # TODO: check values against robotarium
+mpc_distributed.u_max = np.array([1, 1])
+mpc_distributed.x_min = np.array([-10, -10])
+mpc_distributed.x_max = np.array([10, 10])
+mpc_distributed.d_min = 0.2
 
-# TODO: check values against robotarium
-controller.u_min = np.array([-1, -1])
-controller.u_max = np.array([1, 1])
-controller.x_min = np.array([-10, -10])
-controller.x_max = np.array([10, 10])
-controller.d_min = 0.2
+# Initialize helper variables
+x_pred = np.zeros([K, 2, m])
+x_pred_new = np.zeros([K, 2, m])
 
 # Main simulation loop
 T = 50  # total simulation time (s)
-time = np.arange(0, T, dt)
+time = np.arange(0, T + dt, dt)
 for t in time:
 
-    # Collect predicted trajectories from all agents
+    # Compute current and target winding numbers
+    # NOTE: currently the time progresses constantly along the braid
+    tau = t / T  # time n
+    tau_target = (t + K * dt) / T
+    w_curr = windings[int(tau * n_windings), :, :]
+    w_target = windings[int(tau_target * n_windings), :, :]
 
-    # Compute control inputs for all agents
+    # Iterate over agents and solve their local MPC problems and apply control inputs
     for i in range(m):
-        agent = M[i]
-        # controller.solve()  # TODO
+        ego_agent = M[i]
 
-    # Update agents' states based on control inputs
+        # Solve local MPC problem for the ego agent
+        (u_opt, x_opt, cost, t_sol) = mpc_distributed.solve(
+            x_0=ego_agent.x,
+            x_goal=ego_agent.x_goal,
+            x_pred=np.delete(x_pred, i, axis=2),  # remove ego agent's predicted traj
+            w_curr=np.delete(w_curr[i], i, axis=0),  # remove winding number w.r.t. self
+            w_target=np.delete(w_target[i], i, axis=0),
+            use_warm_start=True,
+            sol=ego_agent.sol,
+        )
 
+        # Extract solution and apply control input
+        ego_agent.step(u_opt[0])
+        ego_agent.sol = mpc_distributed.sol
+        x_pred_new[i, 0, :] = ego_agent.x  # save 'true' new state to predicted traj
+        x_pred_new[i, 1:, :] = x_opt[2:, :]  # store predicted traj for next step
+
+        # Print debug info
+        if DEBUG is True:
+            print(f"Time: {t:.2f} s, Agent {i}, Control input: {u_opt[0]}")
+
+    # Update variables for the next iteration
+    x_pred = x_pred_new
 
 # Show all plots
 plt.show()
