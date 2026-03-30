1bf40123829d9a656b0675394c09c02cef0f89e1
gbattocletti <g.battocletti@tudelft.nl>
2026-03-30 15:49:42 +0200

    Update docstring

--- utils/paths2braid.m
+++ utils/paths2braid.m
@@ -1,5 +1,18 @@
 function [braid, word] = paths2braid(paths, angle)
-    % Extracts the braid corresponding to a set of path projected on a plane.
+    % PATHS2BRAID Extracts the braid corresponding to a set of paths projected on a 
+    %   plane.
+    %
+    %   Inputs:
+    %       paths - Paths to project
+    %       angle - Angle specifying the projection plane to generate the braid
+    %
+    %   Output:
+    %       braid - Extracted braid
+    %       word - Word corresponding to the braid
+
+    % Extract the braid from the paths
     braid = compact(braidlab.braid(paths, angle));
+
+    % Extract the braid word
     word = braid.word;
 end
