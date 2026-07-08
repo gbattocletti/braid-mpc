339a60caa65f78f850771be40c7de612c3a761b0
gbattocletti <battocletti.gianpietro@gmail.com>
2026-07-08 15:40:51 +0200

    Customize linewidth

--- results/plot_braid.m
+++ results/plot_braid.m
@@ -6,13 +6,15 @@ clc;
 % Define braid and strand colors
 word = [1, 1, -2];
 colors = {"#a50000", "#ffa217", "#3887ff"};
+linewidth = 1;
 
 % Create figure
 figure('Visible', 'off');
 plot(braid(word));
-hlines = findobj(gca, 'Type', 'line');
-for k = 1:numel(hlines)
-    hlines(k).Color = colors{k};  % apply colors to black strands
+strands = findobj(gca, 'Type', 'line');
+for k = 1:numel(strands)
+    strands(k).Color = colors{k};  % apply colors to black strands
+    strands(k).LineWidth = linewidth;
 end
 axis off;
 
