b39df8d9542d725f7a256e21c8e4ae1d65d4c110
gbattocletti <battocletti.gianpietro@gmail.com>
2026-07-10 09:03:41 +0200

    Move to utils

--- braid_controller/utils/plot_braid.m
+++ braid_controller/utils/plot_braid.m
@@ -4,9 +4,11 @@ close all;
 clc;
 
 % Define braid and strand colors
-word = [1, 1, -2];
-colors = {"#a50000", "#ffa217", "#3887ff"};
-linewidth = 1;
+word = [-8  1  6 -2 -3  7  4  8 -5  3 -6  9  2  1 -4 -8  5  4  7  8  3];
+% word = [1, 1, -2];
+colors = {"#f00000", "#bb7000", "#3887ff", "#b92121", "#ffc814", ...
+          "#0b3b7e", "#9c0000", "#ffb52c", "#0087a8", "#003877"};
+linewidth = 1.5;
 
 % Create figure
 figure('Visible', 'off');
