7211894166c50ea4e8d127ea9f550bb1b24c0467
gbattocletti <battocletti.gianpietro@gmail.com>
2026-07-10 15:24:47 +0200

    Add closure before projection

--- braid_controller/utils/paths2braid.m
+++ braid_controller/utils/paths2braid.m
@@ -1,5 +1,6 @@
 function [braid, word] = paths2braid(paths, angle)
     % Extracts the braid corresponding to a set of path projected on a plane.
+    paths = braidlab.closure(paths);
     braid = compact(braidlab.braid(paths, angle));
     word = braid.word;
 end
